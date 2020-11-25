503bb5eb05c76c0df4cd25fb732aca57639b2217
PUR2212 <59438156+PUR2212@users.noreply.github.com>
2020-11-25 23:43:38 +0530

    initial commit

---
+++ src/main/java/com/example/loginWithSpringBoot/LoginWithSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.loginWithSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginWithSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginWithSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO users (username, password, enabled) values ('purboshi', 'RA1811028010036', true);
+INSERT INTO users (username, password, enabled) values ('user', 'password', true);
+INSERT INTO authorities (username, authority) values ('purboshi', 'ROLE_USER');
+INSERT INTO authorities (username,authority) values ('user', 'ROLE_USER');
